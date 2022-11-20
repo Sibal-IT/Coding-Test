0e3732064dd9d82f0ecba3af1f7645004802760f
yulaula <93673554+yulaula@users.noreply.github.com>
2022-11-20 21:31:49 +0900

    [Solve] ✍ 30

---
+++ zionion/b10610.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class b10610 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] nArr = br.readLine().split("");
+        boolean isZero = false;
+        int sum = 0;
+        Arrays.sort(nArr);
+
+        for(String s : nArr) {
+            sum += Integer.parseInt(s);
+            if(s.equals("0")) {
+                isZero = true;
+            }
+        }
+
+        if(isZero == true && sum%3 == 0) {
+            for(int i = nArr.length-1 ; i >= 0; i--) {
+                System.out.print(nArr[i]);
+            }
+            System.out.println();
+        } else {
+            System.out.println(-1);
+        }
+    }
+}
