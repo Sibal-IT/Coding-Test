b447f541e80627f2ceebd2c8b43ae83ae53e08e0
yulaula <93673554+yulaula@users.noreply.github.com>
2022-12-05 23:19:17 +0900

    [Solve] ✍ 회의실 배정

--- zionion/b2217.java
+++ zionion/b2217.java
@@ -12,7 +12,7 @@ public class b2217 {
             arr[i] = Integer.parseInt(br.readLine());
         }
         Arrays.sort(arr, Collections.reverseOrder());
-
+        
         int max = 0;
         for(int i = 0; i < N; i++) {
             // 로프가 버티는 중량 중 최소값 * 로프의 수 > 현재 최대값
