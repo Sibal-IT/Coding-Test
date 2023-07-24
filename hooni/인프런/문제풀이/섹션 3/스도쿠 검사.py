b07893ac0fae1fa590c922d1db4ae4a5b8bc157a
khoon-git <rudgnssla121@jbnu.ac.kr>
2023-07-25 07:49:00 +0900

    [Solve] ✍ 스도쿠 검사

--- hooni/인프런/문제풀이/섹션 3/스도쿠 검사.py
+++ hooni/인프런/문제풀이/섹션 3/스도쿠 검사.py
@@ -3,10 +3,30 @@ import sys
 
 def main():
     arr = list(list(map(int, input().split())) for _ in range(9))
-
+    flag = True
     for i in range(9):
+        ch1 = [0 for _ in range(10)]
+        ch2 = [0 for _ in range(10)]
         for j in range(9):
-            print("test")
+            ch1[arr[i][j]] = 1
+            ch2[arr[j][i]] = 1
+        if sum(ch1) != 9 or sum(ch2) != 9:
+            flag = False
+    for i in range(0, 3, 6):
+        for j in range(0, 3, 6):
+            ch1 = [0 for _ in range(10)]
+            for dx in range(3):
+                for dy in range(3):
+                    ch1[arr[i + dx][j + dy]] = 1
+            if sum(ch1) != 9:
+                flag = False
+    if flag:
+        print("YES")
+    else:
+        print("NO")            
+    
+
+            
             
     
 
