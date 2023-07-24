7c61a1b4a6939477866a8551b04486fcb5a130f5
rudgnssla121@jbnu.ac.kr <71899948+khoon-git@users.noreply.github.com>
2023-07-25 06:34:14 +0900

    [Solve] ✍ 스도쿠 검사

---
+++ hooni/인프런/문제풀이/섹션 3/스도쿠 검사.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def main():
+    arr = list(list(map(int, input().split())) for _ in range(9))
+
+    for i in range(9):
+        for j in range(9):
+            print("test")
+            
+    
+
+if __name__ == "__main__":
+    main()
