f0c86bfc97cbcd418e56eef5b6c6319fa3698f62
khoon-git <rudgnssla121@jbnu.ac.kr>
2023-08-07 07:45:32 +0900

    [Solve] ✍ 마구간 정하기

--- hooni/인프런/문제풀이/섹션 4/마구간 정하기.py
+++ hooni/인프런/문제풀이/섹션 4/마구간 정하기.py
@@ -5,6 +5,16 @@ def main():
     n, m = map(int, input().split())
     arr = list(int(input()) for _ in range(n))
     arr.sort()
+    lt = 0
+    rt = n
+    while lt <= rt:
+        mid = (lt + rt) // 2
+        flag = False
+        while True:
+            if flag:
+                break
+            for i in range(1, n):
+                if
     
    
 
