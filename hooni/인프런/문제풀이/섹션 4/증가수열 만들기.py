1220aeed49595fd203d88fff92bc661170067ba3
khoon-git <rudgnssla121@jbnu.ac.kr>
2023-08-30 07:43:12 +0900

    [Solve] ✍ 증가수열 만들기

--- hooni/인프런/문제풀이/섹션 4/증가수열 만들기.py
+++ hooni/인프런/문제풀이/섹션 4/증가수열 만들기.py
@@ -4,26 +4,33 @@ import sys
 def main():
     n = int(input())
     arr = list(map(int, input().split()))
-    result = list()
-    if arr[0] > arr[-1]:
-        prev = arr[0]
-        arr.pop(0)
-    elif arr[0] < arr[-1]:   
-    while arr:
-        if len(arr) == 2:
-            if prev
+    prev = 999999999
+    result = ''
+    lt = 0
+    rt = n - 1
+    last = 0
+    tmp = list()
+    while lt <= rt:
+        if last < arr[lt]:
+            tmp.append((arr[lt], 'L'))
+        if last < arr[rt]:
+            tmp.append((arr[rt], 'R'))
+        tmp.sort(key=lambda x:x[0])
+        if len(tmp) == 0:
             break
-        if len(arr) > 1 and arr[0] + arr[-1] <= m:
-            arr.pop(0)
-            arr.pop()
-            cnt += 1
         else:
-            arr.pop()
-            cnt += 1
+            result = result+tmp[0][1]
+            last = tmp[0][0]
+            if tmp[0][1] == 'L':
+                lt = lt+1
+            else:
+                rt = rt-1
+        tmp.clear()
 
     print(len(result))
-    for i in result:
-        print(i, end=' ')
+    print(result)
+    # for i in result:
+    #     print(i, end=' ')
 
 
 if __name__ == "__main__":
