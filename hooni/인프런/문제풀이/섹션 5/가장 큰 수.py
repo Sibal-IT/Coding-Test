1ce72f151bc3036d38b8d3ec403322f615374e5f
khoon-git <rudgnssla121@jbnu.ac.kr>
2023-09-07 07:38:59 +0900

    [Solve] ✍ 가장 큰 수

--- hooni/인프런/문제풀이/섹션 5/가장 큰 수.py
+++ hooni/인프런/문제풀이/섹션 5/가장 큰 수.py
@@ -3,11 +3,18 @@ import sys
 
 def main():
     n, m = map(int, input().split())
-    
-    
+    num = list(map(int, str(n)))
+    result = list()
+    for i in num:
+        while result and m > 0 and result[-1] < i:
+            result.pop()
+            m -= 1
+        result.append(i)
+    if m != 0:
+        result = result[:-m]
 
     for i in result:
-        print(i, end=' ')
+        print(i, end='')
 
 
 if __name__ == "__main__":
