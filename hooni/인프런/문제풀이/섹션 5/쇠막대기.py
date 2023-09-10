5fc2aace8bbecd4618d26f679179e094a8ea2f5f
khoon-git <rudgnssla121@jbnu.ac.kr>
2023-09-11 07:36:20 +0900

    [Solve] ✍ 쇠막대기

--- hooni/인프런/문제풀이/섹션 5/쇠막대기.py
+++ hooni/인프런/문제풀이/섹션 5/쇠막대기.py
@@ -2,20 +2,25 @@ import sys
 
 
 def main():
-    n, m = map(int, input().split())
-    num = list(map(int, str(n)))
-    result = list()
-    for i in num:
-        while result and m > 0 and result[-1] < i:
-            result.pop()
-            m -= 1
-        result.append(i)
-    if m != 0:
-        result = result[:-m]
-
-    for i in result:
-        print(i, end='')
-
+    str_input = input()
+    arr = list(str_input[:])
+    pop_check = False
+    stack = list()
+    cnt = 0
+    ck = 0
+    last_t = ''
+    for i in arr:
+        stack.append(i)
+        if ( stack[-1] == ')' and last_t == '(' ):
+            stack.pop()
+            stack.pop()
+            cnt += len(stack)
+        elif( stack[-1] == ')' and last_t != '(' ):
+            stack.pop()
+            stack.pop()
+            cnt += 1
+        last_t = i
+    print(cnt)
 
 if __name__ == "__main__":
     main()
