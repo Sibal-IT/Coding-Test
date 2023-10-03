44382fe7bd6bc1823362f6f971f973a8d22389b5
khoon-git <rudgnssla121@jbnu.ac.kr>
2023-10-04 07:36:06 +0900

    [Solve] ✍ 공주 구하기

---
+++ hooni/인프런/문제풀이/섹션 5/공주 구하기.py
@@ -0,0 +1,22 @@
+import sys
+
+
+def main():
+    n, k = map(int, input().split())
+    arr = list(x for x in range(1, n - 1))
+    cnt = 0
+    tmp = k
+    while arr:
+        if len(arr) == 1:
+            print(cnt)
+            break
+        if tmp     
+
+    num_list = []
+
+
+    print(int(num_list.pop()))
+
+
+if __name__ == "__main__":
+    main()
