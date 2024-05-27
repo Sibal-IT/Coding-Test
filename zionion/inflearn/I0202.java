1aa900611430bd9e0729bd6c3b6a81bee4044c48
yulaula <ula_ula__@naver.com>
2024-05-27 12:18:25 +0900

    [Solve] ✍ test

---
+++ zionion/inflearn/I0202.java
@@ -0,0 +1,14 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class I0202 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        
+        
+        
+        in.close(); 
+        return ;
+    }
+}
