19745af23de9a5e75094a2b765e4f3dc5a800a62
yulaula <ula_ula__@naver.com>
2024-06-01 16:44:58 +0900

    [Solve] ✍ 보이는 학생

--- zionion/inflearn/I0202.java
+++ zionion/inflearn/I0202.java
@@ -1,11 +1,29 @@
 package inflearn;
 
-import java.util.Scanner;
-
+import java.util.*;
+/*
+* title : 보이는 학생
+* */
 public class I0202 {
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        int cnt = 1;
+        int max = arr[0];
+        for(int i = 1; i < N; i++) {
+            int height = arr[i];
+            if(max < height) {
+                cnt++;
+                max = height;
+            }
+        }
+        System.out.println(cnt);
+
         
         
         in.close(); 
